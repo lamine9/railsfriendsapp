b708edd22b510c02549dc82639af309bdafa8cb5
sibtiger <sallmamadoulamine9@gmail.com>
2023-01-31 16:49:45 +0000

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+    @about_us = 'Doctus impetus iuvaret postulant ut sanctus mandamus. Molestie platonem iuvaret graeco similique ante accommodare hinc. Penatibus pericula esse quisque lorem enim inimicus singulis prodesset vidisse. Posse interdum accumsan ubique risus iudicabit dis sed vidisse tota. Vim congue tota molestie solet salutatus dicit eu verterem vis. Vero mattis option neque reformidans. Propriae dicam tation petentium dolor esse. Fuisset mutat dicunt aliquet risus salutatus sadipscing fugit neque. Scripta class ullamcorper alia pericula ancillae suas. Tincidunt orci possit inceptos omittantur et graecis reprehendunt ullamcorper. Simul utroque fames animal menandri repudiandae at. Commune ancillae impetus mollis reformidans mi graece. Sagittis aeque erroribus venenatis usu elitr repudiandae saperet mattis definitionem. Ei natum egestas epicuri elementum quot legimus dicta. Ullamcorper habemus sumo ignota leo. Singulis volumus debet pulvinar agam nihil. Ad maiorum veritus ne luptatum honestatis massa eleifend vix consul. Labores mnesarchum amet iisque voluptatibus cursus unum. Vitae est causae bibendum dictas bibendum lobortis deserunt honestatis idque. Fames mattis augue adhuc eirmod legimus sem metus. Nunc fermentum tellus quidam ludus appareat feugait homero. Nominavi suspendisse ut aliquam affert torquent cum mentitum. Torquent solum docendi curabitur in. Bibendum quas autem utamur brute. Noluisse imperdiet postulant idque rhoncus.'
+  end
+end
